268efd14ad434a78323eb7cceced5ae94812df4b
Amirhaqbin <95203798+Amirhaqbin@users.noreply.github.com>
2021-12-06 10:46:51 +0330

    new train

--- amirhaqbin/awnsers.py
+++ amirhaqbin/awnsers.py
@@ -1,1 +1,19 @@
-print("awnsers")
+_wallet_value = 100
+
+def wallet_increase(x):
+    global _wallet_value
+    _wallet_value = _wallet_value + x
+
+wallet_increase(10)    
+
+def wallet_decrease(y):
+    global _wallet_value
+    _wallet_value = _wallet_value - y
+    
+wallet_decrease(20)
+ 
+def wallet_value():
+
+    return _wallet_value
+
+print(wallet_value())
