ffc8d4e515b172d24723b4a49b73fdc1e8223fd1
mmd <wadehif452@kyrescu.com>
2021-12-23 14:31:16 +0330

    Final App for Qrcode

--- mmdeshon/push/QR_code generator.py
+++
@@ -1,6 +0,0 @@
-from make_user import create_user
-import qrcode
-user_detail = create_user()
-image = qrcode.make(f"Name is: '{user_detail[0]}'\nUsername is '{user_detail[1]}'\nUserID is '{user_detail[2]}'")
-type(image)
-image.save("User_Details.png")
