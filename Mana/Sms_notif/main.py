1aed2d9b2979d1c09f5d1bdc26689436a8173deb
Mana <manashams94@gmail.com>
2021-12-27 08:15:30 +0330

    sms - push notif

---
+++ Mana/Sms_notif/main.py
@@ -0,0 +1,54 @@
+from users import Users
+from channel import Channel
+
+
+if __name__ == '__main__':
+
+    answer = input('Do you wanna enter your info?[y/n]\n')
+    if answer in ['y', 'Y']:
+        name, email, phone_number, device_id = input('Please enter your name, email, phone number and your device id\n').split()
+        user = Users(name, email, phone_number, device_id)
+        user.save_info()
+
+
+    answer = input('Do you wanna get your info?[y/n]\n')
+    if answer in ['y', 'Y']:
+        sth = input('Enter sth:\n')
+        user = Users().get_info(sth)
+        if user:
+            obj = Users(user['name'], user['email'], user['phone number'], user['device id'])
+            print(Users.show_info(obj))
+        else:
+            print('User does not exist')
+
+
+    answer = input('Do you wanna send push notification?[y/n]\n')
+    if answer in ['y', 'Y']:
+        name = input('Who wanna send push notification?')
+        user = Users().get_info(name)
+
+        if user:
+            device_id = user['device id']
+            obj = Channel()
+            obj.send_push_notification(device_id)
+        else:
+            print('User does not exist')
+
+
+    answer = input('Do you wanna send sms?')
+    if answer in ['y', 'Y']:
+        name = input('Who wanna send sms?')
+        user = Users().get_info(name)
+
+        if user:
+            phone_number = user['phone number']
+            obj = Channel()
+            obj.send_sms(phone_number)
+        else:
+            print('User does not exist')
+
+
+        
+
+    
+    
---
+++ Mana/Sms_notif/users.py
@@ -0,0 +1,39 @@
+import json
+
+
+class Users:
+
+    def __init__(self, name = '', email = '', phone_number = '', device_id = ''):
+        self.name = name
+        self.email = email
+        self.phone_number = phone_number
+        self.device_id = device_id
+
+    def show_info(self):
+        return f'Name: {self.name}\nEmail: {self.email}\nPhone number: {self.phone_number}\nDevice id: {self.device_id}'
+    
+    def save_info(self):
+
+        with open('users.json', 'r+') as json_file:
+    
+            data = json.load(json_file)
+            info = {'name': self.name, 'email': self.email, "phone number": self.phone_number, "device id": self.device_id}
+            data['users'].append(info)
+            json_file.seek(0)
+            json.dump(data, json_file)
+
+    def get_info(self, sth):
+        
+        with open('users.json') as json_file:
+
+            data = json.load(json_file)
+            for user in  data['users']:
+                if sth in user.values():
+                    return user
+                
+
+            
+
+
+        
+
