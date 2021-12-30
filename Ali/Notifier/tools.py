799f178ee7359d49160fb0343434febee94849b5
Ali <Alihosseinzadeh1379ahz@gmail.com>
2021-12-30 17:41:02 +0330

    Notifier app updated (sms and push)

--- Ali/Notifier/tools.py
+++ Ali/Notifier/tools.py
@@ -1,21 +1,44 @@
 import json
+import re
 
 
-def read_from_json(file_name):
-
-    with open(file_name, 'r') as openfile:
-        json_object = json.load(openfile)
-
-    return dict(json_object)
+def validate_email(email):   
     
-            
+    email_pattern = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    while True:
+        if(re.match(email_pattern, email)):   
+            break   
+        else:   
+            email = input('invalid email! please try again : ')
 
-def append_to_json(data_dict, file_name):
-    
-    current_json = dict(read_from_json(file_name))
-    current_json['users'].append(data_dict)
+    return email
 
-    json_object = json.dumps(current_json, indent = 4)
+
+def validate_phonenumber(phone_number):   
     
-    with open(file_name, 'w') as jfile:
-        jfile.write(json_object)
+    pn_pattern = '^09[0-9]{9}$'
+    while True:
+        if(re.match(pn_pattern, phone_number)):   
+            break   
+        else:   
+            phone_number = input('invalid phone number! please try again : ')
+
+    return phone_number
+
+
+def load_from_json(user_phone_number, jfile_name):
+
+    with open(jfile_name, 'r') as f:
+        jsn = dict(json.load(f))
+
+    i = 0
+    while True:
+        
+        if i == len(jsn['users']):
+            print('couln\'t find the user!')
+            return None
+
+        if jsn['users'][i]['phone_number'] == user_phone_number:
+            return jsn['users'][i]['name'], jsn['users'][i]['email'], jsn['users'][i]['phone_number'], jsn['users'][i]['device_id']
+
+        i += 1
